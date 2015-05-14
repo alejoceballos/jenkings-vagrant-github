32ddf4e9f8ed0af00f70aeabbed0a0ca9df3053e
Alejo Ceballos <alejoceballos75@gmail.com>
2015-05-14 00:09:14 -0300

    All Jenkins installation inside a vagrant VM

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# jenkings-vagrant-github
-JENKINS@localhost with HOOK@github.com
+﻿# Jenkings (Ubuntu Vagrant) + Github Hook
+Formerly know as *JENKINS@localhost with HOOK@github*
+
+This project goal is to be used as a tutorial that offers all the necessary infrastructure to start using Jenkins. 
+
+After completing all the steps displayed here you will:
+
++ Have an Ubuntu distro (Trusty/Tahr) running in a Vagrant VM;
++ Have Jenkins running (with all necessary plugins);
++ Have Github's Service Hook rsponsible for triggering Jenkins build.
---
+++ vagrant-jenkins/bootstrap-jenkins.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# Check:
+# http://docs.vagrantup.com/v2/getting-started/provisioning.html
+
+## #############################################################################
+## Jenkins
+## References: 
+## + https://wiki.jenkins-ci.org/display/JENKINS/Installing+Jenkins+on+Ubuntu
+## #############################################################################
+
+echo "## ######################################################################"
+echo "## Jenkins"
+echo "## ######################################################################"
+
+# Add PPA (didn't know this way of adding a PPA)
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+apt-get update
+
+# Install Jenkins
+apt-get -y install jenkins
+
+# ##############################################################################
+# Download and install git plugin & dependencies
+# ##############################################################################
+
+wget -O /var/lib/jenkins/plugins/mailer.hpi "http://updates.jenkins-ci.org/latest/mailer.hpi"
+chown jenkins:jenkins /var/lib/jenkins/plugins/mailer.hpi
+
+# Installed by default
+# "http://updates.jenkins-ci.org/latest/scm-api.hpi"
+
+wget -O /var/lib/jenkins/plugins/matrix-project.hpi "http://updates.jenkins-ci.org/latest/matrix-project.hpi"
+chown jenkins:jenkins /var/lib/jenkins/plugins/matrix-project.hpi
+
+wget -O /var/lib/jenkins/plugins/git-client.hpi "http://updates.jenkins-ci.org/latest/git-client.hpi"
+chown jenkins:jenkins /var/lib/jenkins/plugins/git-client.hpi
+
+# Installed by default
+# http://updates.jenkins-ci.org/latest/ssh-credentials.hpi
+
+# Installed by default
+# http://updates.jenkins-ci.org/latest/credentials.hpi
+
+wget -O /var/lib/jenkins/plugins/git.hpi "http://updates.jenkins-ci.org/latest/git.hpi"
+chown jenkins:jenkins /var/lib/jenkins/plugins/git.hpi
+
+# ##############################################################################
+# Download and install github plugin & dependencies
+# ##############################################################################
+
+wget -O /var/lib/jenkins/plugins/multiple-scms.hpi "http://updates.jenkins-ci.org/latest/multiple-scms.hpi"
+chown jenkins:jenkins /var/lib/jenkins/plugins/multiple-scms.hpi
+
+wget -O /var/lib/jenkins/plugins/github-api.hpi "http://updates.jenkins-ci.org/latest/github-api.hpi"
+chown jenkins:jenkins /var/lib/jenkins/plugins/github-api.hpi
+
+wget -O /var/lib/jenkins/plugins/github.hpi "http://updates.jenkins-ci.org/latest/github.hpi"
+chown jenkins:jenkins /var/lib/jenkins/plugins/github.hpi
+
+# Restart jenkins
+/etc/init.d/jenkins restart
+
+# ##############################################################################
+# Jenkins Project
+#
+# Maven project name: Jack-Ketch-4-Flow
+# _____________________________________
+# Source Code Management: Git
+# -------------------------------------
+# Repository URL: https://github.com/alejoceballos/jack-ketch-4-flow.git
+# Add Credentials
+#   Kind: Username with password
+#   Scope: Global (...)
+#   Username: alejceballos
+#   Password: ************
+# Branch Specifier (blank for 'any'): */master
+# Repository browser: AUto
+# _____________________________________
+# Build
+# -------------------------------------
+# Root POM: jk4j/pom.xml
